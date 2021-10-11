164ecf28f34a158a50675042a22975ffd2090065
Kevin Perez <mr.kv17.kp@gmail.com>
2021-10-11 17:37:28 -0400

    test

--- src/main/java/com/kurodevs/app/backend/controller/ControllerApp.java
+++ src/main/java/com/kurodevs/app/backend/controller/ControllerApp.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://kurodevs.github.io/")
+@CrossOrigin(origins = "*")
 public class ControllerApp {
     
     @GetMapping("/hello")
